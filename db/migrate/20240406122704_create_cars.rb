037cdf4ffbba5c9690948feb71d5b959468a0ef9
Myriam ZAIBET <myriam.zaibet@gmail.com>
2024-04-06 14:37:33 +0200

    création des modèles

---
+++ db/migrate/20240406122704_create_cars.rb
@@ -0,0 +1,16 @@
+class CreateCars < ActiveRecord::Migration[7.1]
+  def change
+    create_table :cars do |t|
+      t.string :model
+      t.string :brand
+      t.integer :year
+      t.integer :km
+      t.text :description
+      t.integer :price_per_day
+      t.string :color
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
