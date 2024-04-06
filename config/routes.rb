d434fc66d646f6eb4c1c94631bf40a9a1953f47e
Myriam ZAIBET <myriam.zaibet@gmail.com>
2024-04-06 15:19:46 +0200

    routes OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "cars#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,4 +9,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :cars do
+    resources :bookings, only: [:create, :new]
+  end
+
+  resources :bookings, only: [:index, :show, :destroy]
 end
